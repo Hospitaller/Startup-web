62c2e5ddd60d9f051ed399e4984ca963a9e511cf
Hospitaller <siribn@yandex.ru>
2018-08-27 17:23:05 +0400

    about us animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+ 
+    var socialBlock = $('.socialBlock_1'),
+    timeoutId;
+$('.aboutUsImg').hover(function(){
+    clearTimeout(timeoutId);
+    socialBlock.show().addClass('animated fadeInUp');
+}, function(){
+    timeoutId = setTimeout($.proxy(socialBlock,'hide'), 300)
+});
+socialBlock.mouseenter(function(){
+    clearTimeout(timeoutId); 
+}).mouseleave(function(){
+    socialBlock.hide();
+}); 
 
- });
+ })
+
+
+
+
