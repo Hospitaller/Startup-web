0d5b07e7af70914adea97eb03601c3cab295f0b2
Hospitaller <siribn@yandex.ru>
2018-08-31 16:46:08 +0400

    animation aboutUsBlock v0.2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+$(document).ready(function(){
+    for (var i = 0; i<arr.length; i++) {
+        arr[i].addClass('animated fadeOutDown');
+    }
 
+});
 
 var arr = [
     $('.socialBlock_1'),
@@ -8,10 +13,33 @@ var arr = [
 ];
 
 function showBlock (i) {
+    clearTimeout(timeoutId);
     var socialBlock = arr[i];
-    socialBlock.show().removeClass('animated fadeOutDown').addClass('animated fadeInUp');
+    socialBlock.show().removeClass('animated fadeOutDown').css('visibility','visible').addClass('animated fadeInUp'),
+    function () {
+        
+    };
 }
 function hideBlock (i){
     var socialBlock = arr[i];
     socialBlock.removeClass('animated fadeInUp').addClass('animated fadeOutDown');
 }
+//*********************************** */
+
+$(document).ready(function() {
+ 
+    var socialBlock = $('.socialBlock_1'),
+    timeoutId;
+$('.aboutUsImg').hover(function(){
+    clearTimeout(timeoutId);
+    socialBlock.show().addClass('animated fadeInUp');
+}, function(){
+    timeoutId = setTimeout($.proxy(socialBlock,'hide'), 300)
+});
+socialBlock.mouseenter(function(){
+    clearTimeout(timeoutId); 
+}).mouseleave(function(){
+    socialBlock.hide();
+}); 
+
+})
