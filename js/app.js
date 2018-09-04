d123409bf3e16fa3aaeae85e947f16339086940f
Hospitaller <siribn@yandex.ru>
2018-09-04 17:38:23 +0400

    with slick slider v0.1

--- js/app.js
+++ js/app.js
@@ -1,42 +1,6 @@
-/*  $(document).ready(function(){
-    for (var i = 0; i<arr.length; i++) {
-        arr[i].addClass('animated fadeOutDown');
-    }
-
-});
-
-var arr = [
-    $('.socialBlock_1'),
-    $('.socialBlock_2'),
-    $('.socialBlock_3'),
-    $('.socialBlock_4'),
-];
-
-function showBlock (i) {
-    var socialBlock = arr[i];
-    socialBlock.show().removeClass('animated fadeOutDown').addClass('animated fadeInUp').css('visibility','visible')
-}
-function hideBlock (i){
-    var socialBlock = arr[i];
-    socialBlock.removeClass('animated fadeInUp').addClass('animated fadeOutDown');
-}
 
 
-var linc2 = $('.socialBlock_1'),
-    timeoutId;
-$('.aboutUsImg').hover(function(){
-    clearTimeout(timeoutId);
-    linc2.show();
-}, function(){
-    timeoutId = setTimeout($.proxy(linc2,'hide'), 1000)
-});
-linc2.mouseenter(function(){
-    clearTimeout(timeoutId); 
-}).mouseleave(function(){
-    linc2.hide();
-}); 
- */
-
+ //Рабочий код
 var coord = 0;
 var block = $('.aboutUsSlider');
 
@@ -50,3 +14,11 @@ function testRight(){
     block.css('left',+coord+'px');
 }
 
+
+				
+$(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
